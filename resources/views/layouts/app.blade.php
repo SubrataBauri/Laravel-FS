9f0a7a2ee72d55ec3a2a18f9aced702ee85eca97
SubrataBauri <subrata@digitalaptech.com>
2018-04-02 00:19:05 +0530

    Compiling assets and create Navbar + add some bootstrap class to tidy up the design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
         <title>{{config('app.name', 'Laravel FS')}}</title>
     </head>
     <body>
-       @yield('content')
+    	@include('inc.navbar')
+    	<div class="container">
+    		@yield('content')
+    	</div>
     </body>
 </html>
