0bf97d8c32b8a312aa72c7f6086c4c5b94a59a0c
SubrataBauri <subrata@digitalaptech.com>
2018-04-01 16:49:45 +0530

    Passing variables from controller to view + pass string variable + pass array variable

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{config('app.name', 'Laravel FS')}}</title>
-    </head>
-    <body>
-       <h1>Welcome to Laravel Blog App!</h1>
+@section('content')
+       <h1>{{$title}}</h1>
        <p>My first Laravel App</p>
-    </body>
-</html>
+@endsection
